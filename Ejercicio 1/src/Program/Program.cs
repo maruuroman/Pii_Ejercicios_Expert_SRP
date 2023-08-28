08835f5cf37e5837c642a68758663daa2e6a7c5a
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-08-28 10:06:42 -0300

    comentarios prueba

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -8,9 +8,9 @@ namespace SRP
         static void Main()
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
-            Book book2 = new Book("Pro C#","Troelsen","001-035");
+            //Book book2 = new Book("Pro C#","Troelsen","001-035");
             book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            //book2.ShelveBook("B","3");
 
         }
     }
